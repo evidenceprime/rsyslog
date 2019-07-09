9aa78328f5acf52e2fedc61714e9f6e520ead46e
Artur Nowak <artur.nowak@evidenceprime.com>
2019-07-09 10:53:11 +0200

    Don't use Upstart on Ubuntu > 14.04

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -2,16 +2,13 @@ module RsyslogCookbook
   # helpers for the various service providers on Ubuntu systems
   module Helpers
     def declare_rsyslog_service
-      if node['platform'] == 'ubuntu' && node['platform_version'].to_f >= 12.04
-        service_provider = Chef::Provider::Service::Upstart
-      else
-        service_provider = nil
-      end
-
       service node['rsyslog']['service_name'] do
         supports :restart => true, :status => true
         action   [:enable, :start]
-        provider service_provider
+        if node['platform'] == 'ubuntu' && node['platform_version'].to_f >= 12.04 &&
+           node['platform_version'].to_f <= 14.04
+          provider Chef::Provider::Service::Upstart
+        end
       end
     end
   end
